33d93dde5240b327c21557bd4bc9f63ea45a688a
Uğur Kerim Sirke <robotdevre@gmail.com>
2025-04-05 10:04:40 +0300

    Create karasimsek.ino

---
+++ karasimsek.ino
@@ -0,0 +1,24 @@
+int ledCount = 8;              // LED sayısı
+int leds[] = {2, 3, 4, 5, 6, 7, 8, 9}; // LED pinleri
+
+void setup() {
+  for (int i = 0; i < ledCount; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+  // Soldan sağa geçiş
+  for (int i = 0; i < ledCount; i++) {
+    digitalWrite(leds[i], HIGH);
+    delay(100);
+    digitalWrite(leds[i], LOW);
+  }
+
+  // Sağdan sola geçiş
+  for (int i = ledCount - 2; i > 0; i--) {
+    digitalWrite(leds[i], HIGH);
+    delay(100);
+    digitalWrite(leds[i], LOW);
+  }
+}
